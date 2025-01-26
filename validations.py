1c3e1bf9cfe916449b8f07c69dffc8e916a96919
98CharleS <karol.sojkowski@gmail.com>
2025-01-26 13:41:45 +0100

    Update validations.py
    Changed aproach to validation.
    Now using datetime.
    Repaired bug in validations func

--- validations.py
+++ validations.py
@@ -3,16 +3,15 @@ import re
 
 
 def raw_date_stripping(date_str):
-    date_pattern = r'^(\d{1,2})[.\-](\d{1,2})[.\-](\d{4})$'
-
-    match = re.match(date_pattern, date_str)
-    if match:
-        day, month, year = map(int, match.groups())
-        # Validate the day and month ranges
-        if 1 <= day <= 31 and 1 <= month <= 12:
-            return day, month, year
-    else:
-        print(f"{date_str} is wrong date")
+    try:
+        # Replace mixed separators with a standard one
+        date_str = re.sub(r'[.\-]', '.', date_str)
+        day, month, year = map(int, date_str.split('.'))
+        # Validate date using datetime
+        datetime(year, month, day)
+        return day, month, year
+    except ValueError:
+        print(f"{date_str} is an invalid date")
         return False
 
 
@@ -20,9 +19,12 @@ def is_first_date_lesser(raw_first_date, raw_second_date):
     date_format = "%Y-%m-%d"
     first_date_couple = raw_date_stripping(raw_first_date)
     second_date_couple = raw_date_stripping(raw_second_date)
-    date1 = f"{first_date_couple[2]}-{first_date_couple[1]}-{first_date_couple[0]}"
-    date2 = f"{second_date_couple[2]}-{second_date_couple[1]}-{second_date_couple[0]}"
-    print(date1, date2)
+    if not first_date_couple or not second_date_couple:
+        print("Error with date stripping")
+        return None  # Invalid input dates
+
+    date1 = f"{first_date_couple[2]}-{first_date_couple[1]:02}-{first_date_couple[0]:02}"
+    date2 = f"{second_date_couple[2]}-{second_date_couple[1]:02}-{second_date_couple[0]:02}"
     try:
         # Parse the input strings into datetime objects
         first_date = datetime.strptime(date1, date_format)
@@ -36,30 +38,29 @@ def is_first_date_lesser(raw_first_date, raw_second_date):
 
 
 def date_validation(first_date, second_date):
-
-    if raw_date_stripping(first_date) and raw_date_stripping(second_date):
-        return True
-    else:
-        return False
+    return bool(raw_date_stripping(first_date) and raw_date_stripping(second_date))
 
 
 def cpv_validation(cpv_code):
     if not cpv_code:
-        # if empty
-        print("Please enter the link code")
+        print("Please enter the CPV code")
         return False
-    elif re.search("[0-9]{8}-[0-9]", cpv_code): # checking if link has correct structure
+    elif re.fullmatch(r"\d{8}-\d", cpv_code):  # Strict match
         return True
     else:
-        print(f'"{cpv_code}" is not valid CPV code\n CPV code should be made of 8 digits, dash and 1 digit\n'
-                  f'Like this: 42111100-1')
+        print(f'"{cpv_code}" is not a valid CPV code.\n'
+              f'A valid CPV code should consist of 8 digits, a dash, and 1 digit (e.g., 42111100-1).')
         return False
 
 
 def validations(day_from, day_to, cpv):
-    if date_validation(day_from, day_to):
-        if is_first_date_lesser(day_from, day_to):
-            if cpv_validation(cpv):
-                return True
-    else:
+    if not date_validation(day_from, day_to):
+        print("One or both dates are invalid")
         return False
+    if not is_first_date_lesser(day_from, day_to):
+        print("The first date is not earlier than the second date")
+        return False
+    if not cpv_validation(cpv):
+        return False
+    return True
+
